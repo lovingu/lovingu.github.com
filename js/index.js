483602352c5927b6cd3bee3af542942a2da4c794
Zuckonit <lsin30@foxmail.com>
2013-08-13 13:23:22 +0800

    ramdom.js added, now random picture can be used

--- js/index.js
+++ js/index.js
@@ -10,18 +10,18 @@
 =============================================================================*/
 var m=0;
 $(document).ready(function(){
-	$(".mindiv").hover(function(){
-		m = $(this).val();
-		if(m>0){
-		$(this).children(".divbox").stop(true, true).animate({"top":"0%"},200); 
-		m=m-1; 
-		}
-		else{
-		$(this).children(".divbox").stop(true, true).animate({"top":"-100%"},200);
-		m=m+1;
-		}
-		
-	},function(){
-		$(this).attr("value",m);
-});
+    $(".mindiv").hover(function(){
+        m = $(this).val();
+        if(m>0){
+            $(this).children(".divbox").stop(true, true).animate({"top":"0%"},200); 
+            m=m-1; 
+        }
+        else{
+            $(this).children(".divbox").stop(true, true).animate({"top":"-100%"},200);
+            m=m+1;
+        }
+
+    },function(){
+        $(this).attr("value",m);
+    });
 });
---
+++ js/random.js
@@ -0,0 +1,25 @@
+var image_count = 12;
+var min = 1;
+var max = 1;
+var fmt = ".png";
+
+function RandomNum() {
+    return Math.random()*(max - min) + min;
+}
+
+function RandomImg(where, sub) {
+    var img_spec = "./images/" + where + "/";
+    var rand_img = img_spec + sub + "/" + RandomNum() + fmt;
+    return rand_img;
+}
+
+for (var i = 1; i <= image_count; i++) {
+    for (var j = 1; j <= 2; j++) {
+        var img = RandomImg(i, j);
+        var box = 'box'+i+'-'+ j;
+        var div = document.getElementById(box);
+        div.style.background = "url(" + img + ")";
+    };
+};
+
+//document.getElementById('box1').style.background = "url(./images/1/1/1.png)";

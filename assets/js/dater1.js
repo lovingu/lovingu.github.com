c6b98509451ea081f3de27a8db4dacee45442a32
lovingu <sin90@foxmail.com>
2013-09-10 08:51:38 +0800

    how many days

---
+++ assets/js/dater1.js
@@ -0,0 +1,34 @@
+var refresher = setInterval(function timeElapse() {
+    var c = new Date();
+    var month = 12;
+    c.setFullYear(2012, month-1, 21);
+    c.setHours(1);
+    c.setMinutes(0);
+    c.setSeconds(0);
+    c.setMilliseconds(0);
+    var e = Date();
+    var f = (Date.parse(e) - Date.parse(c)) / 1000;
+    var g = Math.floor(f / (3600 * 24));
+    f = f % (3600 * 24);
+    var b = Math.floor(f / 3600);
+    if (b < 10) {
+        b = "0" + b;
+    }
+    f = f % 3600;
+    var d = Math.floor(f / 60);
+    if (d < 10) {
+        d = "0" + d;
+    }
+    f = f % 60;
+    if (f < 10) {
+        f = "0" + f;
+    }
+    var theDays = document.getElementById("h_hours");
+    var theHours = document.getElementById("h_days");
+    var theMinutes = document.getElementById("h_minutes");
+    var theSeconds = document.getElementById("h_seconds");
+    theDays.innerHTML = b;
+    theHours.innerHTML = g;
+    theMinutes.innerHTML = d;
+    theSeconds.innerHTML = f;
+}, 1000);
